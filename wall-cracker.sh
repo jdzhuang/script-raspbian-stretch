5695a9f97a00a38c5985ef09505e16aaa5bc2ac0
庄家栋 <jiadong.zhuang@qq.com>
2018-05-06 00:48:08 +0800

    handle error while running in tmux.

--- wall-cracker.sh
+++ wall-cracker.sh
@@ -7,15 +7,19 @@ app_path="$(cd $(dirname $0);pwd)"
 
 _SCRIPT_="\
 function go_ssh(){ \
-	echo 'ssh $O -D $wc_bind $wc_user@$wc_host' ; \
-	ssh $O -D $wc_bind $wc_user@$wc_host ; \
+	echo 'ssh $O -D $wc_bind $wc_ssh_user@$wc_ssh_host' ; \
+	ssh $O -D $wc_bind $wc_ssh_user@$wc_ssh_host ; \
 }; \
 function go_ssh_loop(){ \
 	while true; do go_ssh; done; \
 }; \
 "
 function check_inc(){
-	(test "" == "$wc_user" || test "" == "$wc_host" || test "" == "$wc_bind")&&echo "$app_path/config.inc is not ready.">&2 && exit 1
+	(test "" == "$wc_ssh_user" || test "" == "$wc_ssh_host" || test "" == "$wc_bind")&&echo "$app_path/config.inc is not ready.">&2 && exit 1
+}
+
+function check_tmux(){
+	test "$TMUX_PANE" != "" && echo "tmux is not supported for the moment. plz, try again out of tmux.">&2 && exit 1
 }
 
 function gen_and_nohup(){
@@ -28,10 +32,10 @@ function gen_and_nohup(){
 }
 
 case "$1" in
-	"once") check_inc; gen_and_nohup 'go_ssh' ;;
-	"forever") check_inc; gen_and_nohup 'go_ssh_loop' ;;
-	"stopall") ps -ef|grep -E "$wc_user@$wc_host|$app_name" |grep -v "grep"| awk '{print $2}' |xargs kill ;;
-	"check") ps -ef|grep -E "$wc_user@$wc_host|$app_name" |grep -v "grep"| grep -v "check" ;;
+	"once") check_tmux; check_inc; gen_and_nohup 'go_ssh' ;;
+	"forever") check_tmux; check_inc; gen_and_nohup 'go_ssh_loop' ;;
+	"stopall") ps -ef|grep -E "$wc_ssh_user@$wc_ssh_host|$app_name" |grep -v "grep"| awk '{print $2}' |xargs kill ;;
+	"check") ps -ef|grep -E "$wc_ssh_user@$wc_ssh_host|$app_name" |grep -v "grep"| grep -v "check" ;;
 	*) echo "usage: $0 check|once|forever|stopall">&2; exit 1 ;;
 esac
 
